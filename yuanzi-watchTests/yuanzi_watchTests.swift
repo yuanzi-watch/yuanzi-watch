6c759316e60d6f3f5965468ef2786ad29e60bfef
wangzhiyuan123 <415176294@qq.com>
2017-03-06 14:32:37 +0800

    Initial Commit

---
+++ yuanzi-watchTests/yuanzi_watchTests.swift
@@ -0,0 +1,36 @@
+//
+//  yuanzi_watchTests.swift
+//  yuanzi-watchTests
+//
+//  Created by wzy on 17/3/6.
+//  Copyright © 2017年 wzy. All rights reserved.
+//
+
+import XCTest
+@testable import yuanzi_watch
+
+class yuanzi_watchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
